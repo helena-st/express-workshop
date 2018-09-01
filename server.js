3f6053fc2ba72fb4fe5058ac3f2aff79f6ebb9b1
helena-st <helena@lifesum.com>
2018-09-01 14:17:35 +0200

    add post functionality writing to json file

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var formidable = require('express-formidable');
+//app.use(express.static("public"));
 
+//app.use(formidable());
+app.use(express.static("public"), formidable());
 
 app.listen(3000, function(){
   console.log('server is listening on port 3000. Allons-y!')
 })
 
 
-app.get("/", function(req, res){
-  res.send("Yay Node Girls!");
+
+app.post("/create-post", function(req, res){
+  var blogText = req.fields
+  readAndWrite(blogText)
 })
 
-app.get("/node", function(req, res){
-  res.send("hello?");
-})
+function readAndWrite(blogText){
+  fs.readFile(__dirname + '/data/posts.json', function(error, file){
+    var parsedFile = JSON.parse(file)
+    var timestamp = Date.now()
+    parsedFile[timestamp] = blogText['blogpost']
+    var textToWrite = JSON.stringify(parsedFile, null, 4)
+    fs.writeFile(__dirname + '/data/posts.json', textToWrite, function(error){
+      console.log(parsedFile);
+    })
+  });
 
-app.get("/girls", function(req, res){
-  res.send("Who run the world?");
-})
+}
+// fs.readFile(__dirname + '/data/posts.json', function(error, file){
+//   var parsedFile = JSON.parse(file)
+//   console.log(parsedFile);
+// });
